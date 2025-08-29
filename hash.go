af86c35d7e0c92d672e1a46a9d48128d5195da4a
pefish <darrondarron@qq.com>
2025-08-29 15:48:11 +0800

    v0.8.4

--- hash.go
+++ hash.go
@@ -168,3 +168,11 @@ func (rc *HashType) Values(key string) ([]string, error) {
 	}
 	return result, nil
 }
+
+func (rc *HashType) IncrBy(key string, field string, increment int64) error {
+	rc.logger.DebugF(`Redis HIncrBy. key: %s, field: %s, increment: %f`, key, field, increment)
+	if err := rc.db.HIncrBy(context.Background(), key, field, increment).Err(); err != nil {
+		return errors.Wrapf(err, "<key: %s>", key)
+	}
+	return nil
+}
