157087d5efd52bb38ba24f066259f6f89014c0ac
pefish <pefish@pefishs-MacBook-Pro.local>
2025-10-25 14:00:01 +0800

    add

--- _example/main.go
+++ _example/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	i_logger "github.com/pefish/go-interface/i-logger"
 	go_redis "github.com/pefish/go-redis"
 )
 
 func main() {
+	envMap, _ := godotenv.Read("./.env")
+	for k, v := range envMap {
+		os.Setenv(k, v)
+	}
+
 	err := do()
 	if err != nil {
 		log.Fatal(err)
@@ -18,16 +25,14 @@ func main() {
 func do() error {
 	instance := go_redis.NewRedisInstance(&i_logger.DefaultLogger)
 	err := instance.Connect(&go_redis.Configuration{
-		Url:      "127.0.0.1",
-		Password: "password",
+		Url:      os.Getenv("REDIS_URL"),
+		Password: os.Getenv("REDIS_PASSWORD"),
 	})
 	if err != nil {
 		return err
 	}
 
-	key := "sdfgadfg11"
-	// instance.Hash.SetUint64(key, "111", 111)
-	r, err := instance.Hash.GetUint64(key, "111")
+	r, err := instance.String.Get("test")
 	if err != nil {
 		return err
 	}
